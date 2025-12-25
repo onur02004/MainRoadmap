54356f972eca2db6f8b4ab9d2e3d804ca6aa83e4
330nur <66780406+onur02004@users.noreply.github.com>
2025-12-25 15:12:27 +0100

    song chare notification bug fix v2

--- src/constants/notificationTypes.js
+++ src/constants/notificationTypes.js
@@ -1,9 +1,6 @@
-export const NotificationType = Object.freeze({
-  LOGIN: "login",
-  PUBLIC_SHARE: "public_share",
-  DIRECT_SHARE: "direct_share",
-  SONG_PLAYED: "song_played",
-  REACTION: 'reaction',
-  COMMENT: 'comment',
-  PLAY_EVENT: 'play_event'
-});
+export const NotificationType = {
+    DIRECT_SHARE: 'direct_share',
+    REACTION: 'reaction',   // Must match DB exactly
+    COMMENT: 'comment',     // Must match DB exactly
+    PLAY_EVENT: 'play_event' // Must match DB exactly
+};
